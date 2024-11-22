fa010e398e31bf11a1fd1356511ae5062a72eba5
lneuendorf <lneuendorf@wisc.edu>
2024-11-21 19:24:02 -0600

    working on plotting vertical direction

---
+++ README.md
@@ -0,0 +1,6 @@
+# Todo
+1. look into plays where there are motions and shifts, do defenders shift in response? Do they shift to fill gaps?
+
+
+# Notes:
+* The motion and shift events are human tagged, whereas the motion and shift binary player play columns are tagged via a rule-based process
--- py/util.py
+++ py/util.py
@@ -1,28 +1,76 @@
 import re
+from typing import Tuple
+
 import pandas as pd
 import numpy as np
 
-def standardize_direction(df_tracking, df_play):
+def standardize_direction(
+    df_tracking: pd.DataFrame,
+    df_play: pd.DataFrame
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Standardize the direction of the play and the players to be vertical.
+
+    The direction of the play is set to be bottom to top, with the offensive
+    moving from the bottom to the top.
+
+    Args:
+        df_tracking: The tracking data.
+        df_play: The play data.
+
+    Returns:
+        The tracking data and the play data with the direction standardized.
+    """
     
     left_play = df_tracking['play_direction'] == 'left'
     
-    df_tracking['x'] = np.where(left_play, 120 - df_tracking['x'], df_tracking['x'])
-    df_tracking['y'] = np.where(left_play, 53.3 - df_tracking['y'], df_tracking['y'])
-    df_tracking['dir'] = np.where(left_play, (df_tracking['dir'] - 180) % 360, df_tracking['dir'])
-    df_tracking['o'] = np.where(left_play, (df_tracking['o'] - 180) % 360, df_tracking['o'])
+    original_x = df_tracking['x'].copy()
+    original_y = df_tracking['y'].copy()
+    
+    df_tracking['y'] = np.where(
+        left_play, 
+        120 - original_x,
+        original_x
+    )
+    df_tracking['x'] = np.where(
+        left_play, 
+        original_y,
+        53.3 - original_y
+    )
 
-    # flip the degree axis system to be in alignment with the radian circle
-    df_tracking['dir'] = (90 - df_tracking['dir']) % 360
-    df_tracking['o'] = (90 - df_tracking['o']) % 360
+    #TODO: figure out if you need to flip the direction of the players 
+    # df_tracking['dir'] = np.where(
+    #     left_play, 
+    #     (df_tracking['dir'] - 90) % 360, 
+    #     (df_tracking['dir'] + 90) % 360
+    # )
+    # df_tracking['o'] = np.where(
+    #     left_play, 
+    #     (df_tracking['o'] - 90) % 360, 
+    #     (df_tracking['o'] + 90) % 360
+    # )
 
-    key = ['game_id','play_id']
-    df_play = df_play.merge(df_tracking[key + ['play_direction']], how='left', on=key)
-    df_play['absolute_yardline_number'] = np.where(df_play.play_direction == "left", 
-                                                   120 - df_play.absolute_yardline_number, 
-                                                   df_play.absolute_yardline_number)
+    df_tracking['dir'] = (180 - df_tracking['dir']) % 360
+    df_tracking['o'] = (180 - df_tracking['o']) % 360
+
+    key = ['game_id', 'play_id']
+    df_play = df_play.merge(
+        right = df_tracking.drop_duplicates(key)[key + ['play_direction']], 
+        how = 'left', 
+        on = key
+    )
+
+    # Drop plays that arent in the tracking data
+    df_play = df_play.dropna(subset=['play_direction']).reset_index(drop=True)
+
+    df_play['absolute_yardline_number'] = np.where(
+        df_play.play_direction == "left", 
+        120 - df_play.absolute_yardline_number, 
+        df_play.absolute_yardline_number
+    )
+    
     df_play.drop('play_direction', axis=1)
 
     return df_tracking, df_play
 
-def uncamelcase_columns(df):
+def uncamelcase_columns(df: pd.DataFrame) -> None:
     df.columns = [re.sub(r'(?<!^)(?=[A-Z])', '_', word).lower() for word in df.columns]
